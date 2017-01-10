b5dc82b7b5d283578c0c6fd2cdd73257260d08f7
ARIEL REY <ariel.rey@mercadolibre.com>
2017-01-10 17:58:07 -0300

    Classes: Adding remaining schemas

--- test/validation.js
+++ test/validation.js
@@ -11,6 +11,10 @@ describe('Validation Module', function(){
                 },
                 "last_name": {
                     "type": "string"
+                },
+                "creation_date": {
+                    "type": "string",
+                    "pattern": "[0-9]{4}\-[0-9]{2}\-[0-9]{2}T[0-9]{2}\:[0-9]{2}\:[0-9]{2}\.[0-9]{3}\-[0-9]{2}\:[0-9]{2}$"
                 }
             }
         };
@@ -43,5 +47,23 @@ describe('Validation Module', function(){
         assert.isArray(errors, 'Always returns an array');
         assert.equal(errors.length, 1, 'Should be an error');
         assert.equal(message, 'The next fields are failing on validation: ".first_name": should be string.')
+    });
+
+    it('Valid ISO 8601 Pattern', function(){
+        var errors = validationModule.validate(testSchema, {
+            creation_date: '2016-01-01T18:00:00.000-03:00'
+        });
+
+        assert.isArray(errors, 'Always returns an array');
+        assert.equal(errors.length, 0, 'Shouldnt be any errors');
+    });
+
+    it('Invalid ISO 8601 Pattern', function(){
+        var errors = validationModule.validate(testSchema, {
+            creation_date: '2016-01-01T18:00:00.000'
+        });
+
+        assert.isArray(errors, 'Always returns an array');
+        assert.equal(errors.length, 1, 'Should be an error');
     });
 });
