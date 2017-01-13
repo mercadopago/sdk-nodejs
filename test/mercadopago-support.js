5c5b76f500743c340279731c86f31d5c285305eb
ARIEL REY <ariel.rey@mercadolibre.com>
2017-01-13 15:58:21 -0300

    Compatiblity: Adding warnings. Adding tests

--- test/mercadopago-support.js
+++ test/mercadopago-support.js
@@ -21,236 +21,316 @@ describe('Mercadopago SDK', function(){
         firstname: 'Ariel'
     };
 
-    beforeEach(function(){
-        requestStub = sinon.stub(requestLib, 'Request', function(params){
-            return params.callback.apply(null, [null, {
-                statusCode: 200
-            }, mpResponse]);
-        });
-    });
+    it('Show Warning', function(){
+        var stub = sinon.stub(console, 'warn', function(){ /* Do Nothing */});
 
-    afterEach(function(){
-        requestStub.restore();
-    });
-
-    it('SandboxMode', function(){
         mp.sandboxMode(true);
 
-        assert.isTrue(mp.configurations.sandbox);
+        assert.isTrue(stub.called);
 
-        mp.sandboxMode();
-
-        assert.isTrue(mp.configurations.sandbox);
-
-        mp.sandboxMode(false);
-
-        assert.isFalse(mp.configurations.sandbox);
+        stub.restore();
     });
 
-    it('get', function(){
-        var promise = mp.get('/v1/payments/1', {
-            test_paremeter: 'get'
+    describe('Methods without warning', function(){
+        var consoleStub;
+
+        before(function(){
+            consoleStub = sinon.stub(console, 'warn', function(){ /* Do Nothing */});
         });
 
-        assert.isFulfilled(promise);
-
-        var requestArgs = requestStub.args[0][0];
-
-        assert.isTrue(requestStub.called);
-        assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/payments/1');
-        assert.equal(requestArgs.method, 'GET');
-        assert.equal(requestArgs.qs.test_paremeter, 'get');
-
-        var callback = sinon.spy();
-
-        var promiseCallback = mp.get('/v1/payments/1', callback);
-
-        assert.isFulfilled(promiseCallback);
-
-        assert.isTrue(callback.called);
-    });
-
-    it('delete', function(){
-        var promise = mp.delete('/v1/payments/1', {
-            test_paremeter: 'delete'
+        after(function(){
+            consoleStub.restore();
         });
 
-        assert.isFulfilled(promise);
-
-        var requestArgs = requestStub.args[0][0];
-
-        assert.isTrue(requestStub.called);
-        assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/payments/1');
-        assert.equal(requestArgs.method, 'DELETE');
-        assert.equal(requestArgs.qs.test_paremeter, 'delete');
-
-        var callback = sinon.spy();
-
-        var promiseCallback = mp.delete('/v1/payments/1', callback);
-
-        assert.isFulfilled(promiseCallback);
-
-        assert.isTrue(callback.called);
-    });
-
-    it('post', function(){
-        var promise = mp.post('/v1/payments', {
-            payload: true
-        },{
-            test_paremeter: 'post'
+        beforeEach(function(){
+            requestStub = sinon.stub(requestLib, 'Request', function(params){
+                return params.callback.apply(null, [null, {
+                    statusCode: 200
+                }, mpResponse]);
+            });
         });
 
-        assert.isFulfilled(promise);
-
-        var requestArgs = requestStub.args[0][0];
-
-        assert.isTrue(requestStub.called);
-        assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/payments');
-        assert.equal(requestArgs.method, 'POST');
-        assert.isTrue(requestArgs.json.payload);
-        assert.equal(requestArgs.qs.test_paremeter, 'post');
-
-        var callback = sinon.spy();
-
-        var promiseCallback = mp.post('/v1/payments', callback);
-
-        assert.isFulfilled(promiseCallback);
-
-        assert.isTrue(callback.called);
-    });
-
-    it('put', function(){
-        var promise = mp.put('/v1/payments', {
-            payload: true
-        },{
-            test_paremeter: 'put'
+        afterEach(function(){
+            requestStub.restore();
         });
 
-        assert.isFulfilled(promise);
+        it('SandboxMode', function(){
+            mp.sandboxMode(true);
 
-        var requestArgs = requestStub.args[0][0];
+            assert.isTrue(mp.configurations.sandbox);
 
-        assert.isTrue(requestStub.called);
-        assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/payments');
-        assert.equal(requestArgs.method, 'PUT');
-        assert.isTrue(requestArgs.json.payload);
-        assert.equal(requestArgs.qs.test_paremeter, 'put');
+            mp.sandboxMode();
 
-        var callback = sinon.spy();
+            assert.isTrue(mp.configurations.sandbox);
 
-        var promiseCallback = mp.put('/v1/payments', callback);
+            mp.sandboxMode(false);
 
-        assert.isFulfilled(promiseCallback);
-
-        assert.isTrue(callback.called);
-    });
-
-    it('updatePreference', function(){
-        var stub = sinon.stub(preferencesModule, 'update', function(){});
-
-        mp.updatePreference(1, {});
-
-        assert.isTrue(stub.called);
-        assert.isTrue(stub.calledWith({ id: 1 }));
-
-        stub.restore();
-    });
-
-    it('updatePreapprovalPayment', function(){
-        var stub = sinon.stub(preapprovalModule, 'update', function(){});
-
-        mp.updatePreapprovalPayment(1, {});
-
-        assert.isTrue(stub.called);
-        assert.isTrue(stub.calledWith({ id: 1 }));
-
-        stub.restore();
-    });
-
-    it('searchPayment', function(){
-        var stub = sinon.stub(paymentModule, 'search', function(){});
-
-        mp.searchPayment({ fields: 'firstname' }, 0, 10);
-
-        assert.isTrue(stub.called);
-        assert.isTrue(stub.calledWith({ qs: { fields: 'firstname', offset: 0, limit: 10 } }));
-
-        mp.searchPayment({ fields: 'firstname' });
-
-        assert.isTrue(stub.called);
-        assert.isTrue(stub.calledWith({ qs: { fields: 'firstname' } }));
-
-        stub.restore();
-    });
-
-    it('getPayment', function(){
-        var stub = sinon.stub(ipnModule, 'getPayment', function(){});
-
-        mp.getPayment(1);
-
-        assert.isTrue(stub.called);
-        assert.isTrue(stub.calledWith(1, undefined));
-
-        stub.restore();
-    });
-
-    it('getAuthorizedPayment', function(){
-        var stub = sinon.stub(ipnModule, 'getAuthorizedPayment', function(){});
-
-        mp.getAuthorizedPayment(1);
-
-        assert.isTrue(stub.called);
-        assert.isTrue(stub.calledWith(1, undefined));
-
-        stub.restore();
-    });
-
-    it('refundPayment', function(){
-        var stub = sinon.stub(requestManager, 'generateAccessToken', function(callback){
-            callback.apply(null, [null, 'ACCESS_TOKEN']);
+            assert.isFalse(mp.configurations.sandbox);
         });
 
-        mp.refundPayment(1);
+        it('getAccessToken', function(){
+            var stub = sinon.stub(requestManager, 'generateAccessToken', function(callback){
+                callback.apply(null, [null, 'ACCESS_TOKEN']);
+            });
 
-        var requestArgs = requestStub.args[0][0];
+            var callback = sinon.spy();
 
-        assert.isTrue(requestStub.called);
+            mp.getAccessToken(callback);
 
-        assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/collections/1');
-        assert.equal(requestArgs.method, 'PUT');
-        assert.equal(requestArgs.json.id, 1);
-        assert.equal(requestArgs.json.status, 'refunded');
+            assert.isTrue(callback.called);
 
-        stub.restore();
-    });
-
-    it('cancelPayment', function(){
-        var stub = sinon.stub(requestManager, 'generateAccessToken', function(callback){
-            callback.apply(null, [null, 'ACCESS_TOKEN']);
+            stub.restore();
         });
 
-        mp.cancelPayment(1);
+        it('get', function(){
+            var promise = mp.get('/v1/payments/1', {
+                test_paremeter: 'get'
+            });
 
-        var requestArgs = requestStub.args[0][0];
+            assert.isFulfilled(promise);
 
-        assert.isTrue(requestStub.called);
+            var requestArgs = requestStub.args[0][0];
 
-        assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/collections/1');
-        assert.equal(requestArgs.method, 'PUT');
-        assert.equal(requestArgs.json.id, 1);
-        assert.equal(requestArgs.json.status, 'cancelled');
+            assert.isTrue(requestStub.called);
+            assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/payments/1');
+            assert.equal(requestArgs.method, 'GET');
+            assert.equal(requestArgs.qs.test_paremeter, 'get');
 
-        stub.restore();
-    });
+            var callback = sinon.spy();
 
-    it('cancelPreapprovalPayment', function(){
-        var stub = sinon.stub(preapprovalModule, 'update', function(){});
+            var promiseCallback = mp.get('/v1/payments/1', callback);
 
-        mp.cancelPreapprovalPayment(1);
+            assert.isFulfilled(promiseCallback);
 
-        assert.isTrue(stub.called);
-        assert.isTrue(stub.calledWith({ id: 1, status: "cancelled" }, undefined));
+            assert.isTrue(callback.called);
+        });
 
-        stub.restore();
+        it('post', function(){
+            var promise = mp.post('/v1/payments', {
+                payload: true
+            },{
+                test_paremeter: 'post'
+            });
+
+            assert.isFulfilled(promise);
+
+            var requestArgs = requestStub.args[0][0];
+
+            assert.isTrue(requestStub.called);
+            assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/payments');
+            assert.equal(requestArgs.method, 'POST');
+            assert.isTrue(requestArgs.json.payload);
+            assert.equal(requestArgs.qs.test_paremeter, 'post');
+
+            var callback = sinon.spy();
+
+            var promiseCallback = mp.post('/v1/payments', callback);
+
+            assert.isFulfilled(promiseCallback);
+
+            assert.isTrue(callback.called);
+        });
+
+        it('put', function(){
+            var promise = mp.put('/v1/payments', {
+                payload: true
+            },{
+                test_paremeter: 'put'
+            });
+
+            assert.isFulfilled(promise);
+
+            var requestArgs = requestStub.args[0][0];
+
+            assert.isTrue(requestStub.called);
+            assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/payments');
+            assert.equal(requestArgs.method, 'PUT');
+            assert.isTrue(requestArgs.json.payload);
+            assert.equal(requestArgs.qs.test_paremeter, 'put');
+
+            var callback = sinon.spy();
+
+            var promiseCallback = mp.put('/v1/payments', callback);
+
+            assert.isFulfilled(promiseCallback);
+
+            assert.isTrue(callback.called);
+        });
+
+        it('delete', function(){
+            var promise = mp.delete('/v1/payments/1', {
+                test_paremeter: 'delete'
+            });
+
+            assert.isFulfilled(promise);
+
+            var requestArgs = requestStub.args[0][0];
+
+            assert.isTrue(requestStub.called);
+            assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/payments/1');
+            assert.equal(requestArgs.method, 'DELETE');
+            assert.equal(requestArgs.qs.test_paremeter, 'delete');
+
+            var callback = sinon.spy();
+
+            var promiseCallback = mp.delete('/v1/payments/1', callback);
+
+            assert.isFulfilled(promiseCallback);
+
+            assert.isTrue(callback.called);
+        });
+
+        it('createPreference', function(){
+            var stub = sinon.stub(preferencesModule, 'create', function(){});
+
+            mp.createPreference({ test: true });
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith({ test: true }, undefined));
+
+            stub.restore();
+        });
+
+        it('updatePreference', function(){
+            var stub = sinon.stub(preferencesModule, 'update', function(){});
+
+            mp.updatePreference(1, {});
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith({ id: 1 }));
+
+            stub.restore();
+        });
+
+        it('getPreference', function(){
+            var stub = sinon.stub(preferencesModule, 'get', function(){});
+
+            mp.getPreference(1);
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith(1, undefined));
+
+            stub.restore();
+        });
+
+        it('createPreapprovalPayment', function(){
+            var stub = sinon.stub(preapprovalModule, 'create', function(){});
+
+            mp.createPreapprovalPayment({ id: 1 });
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith({ id: 1 }, undefined));
+
+            stub.restore();
+        });
+
+        it('updatePreapprovalPayment', function(){
+            var stub = sinon.stub(preapprovalModule, 'update', function(){});
+
+            mp.updatePreapprovalPayment(1, {});
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith({ id: 1 }));
+
+            stub.restore();
+        });
+
+        it('getPreapprovalPayment', function(){
+            var stub = sinon.stub(preapprovalModule, 'get', function(){});
+
+            mp.getPreapprovalPayment(1);
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith(1, undefined));
+
+            stub.restore();
+        });
+
+        it('searchPayment', function(){
+            var stub = sinon.stub(paymentModule, 'search', function(){});
+
+            mp.searchPayment({ fields: 'firstname' }, 0, 10);
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith({ qs: { fields: 'firstname', offset: 0, limit: 10 } }));
+
+            mp.searchPayment({ fields: 'firstname' });
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith({ qs: { fields: 'firstname' } }));
+
+            stub.restore();
+        });
+
+        it('getPayment', function(){
+            var stub = sinon.stub(ipnModule, 'getPayment', function(){});
+
+            mp.getPayment(1);
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith(1, undefined));
+
+            stub.restore();
+        });
+
+        it('getAuthorizedPayment', function(){
+            var stub = sinon.stub(ipnModule, 'getAuthorizedPayment', function(){});
+
+            mp.getAuthorizedPayment(1);
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith(1, undefined));
+
+            stub.restore();
+        });
+
+        it('refundPayment', function(){
+            var stub = sinon.stub(requestManager, 'generateAccessToken', function(callback){
+                callback.apply(null, [null, 'ACCESS_TOKEN']);
+            });
+
+            mp.refundPayment(1);
+
+            var requestArgs = requestStub.args[0][0];
+
+            assert.isTrue(requestStub.called);
+
+            assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/collections/1');
+            assert.equal(requestArgs.method, 'PUT');
+            assert.equal(requestArgs.json.id, 1);
+            assert.equal(requestArgs.json.status, 'refunded');
+
+            stub.restore();
+        });
+
+        it('cancelPayment', function(){
+            var stub = sinon.stub(requestManager, 'generateAccessToken', function(callback){
+                callback.apply(null, [null, 'ACCESS_TOKEN']);
+            });
+
+            mp.cancelPayment(1);
+
+            var requestArgs = requestStub.args[0][0];
+
+            assert.isTrue(requestStub.called);
+
+            assert.equal(requestArgs.uri, mp.configurations.getBaseUrl() + '/v1/collections/1');
+            assert.equal(requestArgs.method, 'PUT');
+            assert.equal(requestArgs.json.id, 1);
+            assert.equal(requestArgs.json.status, 'cancelled');
+
+            stub.restore();
+        });
+
+        it('cancelPreapprovalPayment', function(){
+            var stub = sinon.stub(preapprovalModule, 'update', function(){});
+
+            mp.cancelPreapprovalPayment(1);
+
+            assert.isTrue(stub.called);
+            assert.isTrue(stub.calledWith({ id: 1, status: "cancelled" }, undefined));
+
+            stub.restore();
+        });
     });
 });
