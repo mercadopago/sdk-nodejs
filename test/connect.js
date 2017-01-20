1c5186e684ada321f75d33d5f8eff81c44debb18
ARIEL REY <ariel.rey@mercadolibre.com>
2017-01-20 16:36:23 -0300

    Connect: Adding method to get user credentials and configure them into configurations

--- test/connect.js
+++ test/connect.js
@@ -4,6 +4,8 @@ var assert = chai.assert;
 var chaiAsPromised = require('chai-as-promised');
 var Promise = require('bluebird');
 var connectModule = require('../lib/resources/connect');
+var requestManager = require('../lib/request-manager');
+var configurations = require('../lib/configurations');
 
 chai.use(chaiAsPromised);
 
@@ -42,5 +44,81 @@ describe('Connect', function () {
     });
 
     getAuthroizationCodeStub.restore();
+  });
+
+  it('getCredentialsAndConfigure', function () {
+    var promise;
+    var promiseWithoutCallback;
+    var callback;
+    var userCredentialsResponse = {
+      status: 200,
+      body: {
+        access_token: 'USER_AT',
+        public_key: 'USER_PK',
+        refresh_token: 'USER_RT',
+        live_mode: false,
+        user_id: 123456789,
+        token_type: 'bearer',
+        expires_in: 15768000,
+        scope: 'offline_access read write'
+      }
+    };
+
+    callback = sinon.spy();
+
+    var getUserCredentialsStub = sinon.stub(requestManager, 'getUserCredentials', function (config, funcCallback) {
+      return new Promise(function (resolve) {
+        resolve(userCredentialsResponse);
+        return funcCallback.apply(null, [null, userCredentialsResponse]);
+      });
+    });
+
+    promise = connectModule.getCredentialsAndConfigure('clientSecret', 'authorizationCode', 'redirectURI', callback);
+
+    assert.isFulfilled(promise, userCredentialsResponse);
+
+    promise.then(function () {
+      assert.isTrue(callback.called);
+      assert.isTrue(callback.calledWith(null, userCredentialsResponse));
+      assert.equal(configurations.getAccessToken(), userCredentialsResponse.body.access_token);
+    });
+
+    promiseWithoutCallback = connectModule.getCredentialsAndConfigure('clientSecret', 'authorizationCode', 'redirectURI');
+
+    assert.isFulfilled(promiseWithoutCallback, userCredentialsResponse);
+
+    promiseWithoutCallback.then(function () {
+      assert.equal(configurations.getAccessToken(), userCredentialsResponse.body.access_token);
+    });
+
+    getUserCredentialsStub.restore();
+  });
+
+  it('Fail getCredentialsAndConfigure', function () {
+    var promise;
+    var callback;
+    var errorMessage = 'Error Ocurred';
+
+    callback = sinon.spy();
+
+    var getUserCredentialsStub = sinon.stub(requestManager, 'getUserCredentials', function (config, funcCallback) {
+      return new Promise(function (resolve, reject) {
+        reject(new Error(errorMessage));
+        return funcCallback.apply(null, [new Error(errorMessage), null]);
+      });
+    });
+
+    promise = connectModule.getCredentialsAndConfigure('clientSecret', 'authorizationCode', 'redirectURI', callback);
+
+    assert.isRejected(promise);
+
+    promise.catch(function () {
+      var callbackError = callback.args[0][0];
+
+      assert.isTrue(callback.called);
+      assert.equal(callbackError.message, errorMessage);
+    });
+
+    getUserCredentialsStub.restore();
   });
 });
