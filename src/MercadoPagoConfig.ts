bda02c68da25ba3320c6639730c6d9a7e873b641
“Lucas <lucas.duarte@mercadolivre.com>
2023-08-08 16:34:48 -0300

    search emthod implementation

---
+++ src/MercadoPagoConfig.ts
@@ -0,0 +1,11 @@
+import type { Config, Options } from './types';
+
+export class MercadoPagoConfig {
+  accessToken: string;
+  options: Options;
+
+  constructor(config: Config) {
+    this.accessToken = config.accessToken;
+    this.options = config.options;
+  }
+}
