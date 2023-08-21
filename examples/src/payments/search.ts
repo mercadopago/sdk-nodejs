e12b9001cf012abdc4ae4a4ff7c765394cc99821
“Lucas <lucas.duarte@mercadolivre.com>
2023-08-21 13:53:21 -0300

    eslint on examples

--- examples/src/payments/search.ts
+++ examples/src/payments/search.ts
@@ -1,9 +1,9 @@
 import MercadoPago, { Payments } from '../../../dist';
 
-const client = new MercadoPago({accessToken: ''})
+const client = new MercadoPago({accessToken: ''});
 
 const payments = new Payments(client);
 
 payments.search({
-    criteria: 'asc'
-}).then((result) => console.log(result.results[0]))
+	criteria: 'asc'
+}).then((result) => console.log(result.results[0]));
