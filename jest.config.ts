bb9da80cccf06149b3314993c4d6456cf0c94c65
“Lucas <lucas.duarte@mercadolivre.com>
2023-08-21 13:20:25 -0300

    finish config jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,17 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
 	clearMocks: true,
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	coverageProvider: 'v8',
+	coverageReporters: [
+		'html',
+		'text-summary'
+	],
+	testMatch: [
+		'**/?(*.)+(spec|test).[tj]s?(x)'
+	],
+	transform: {'^.+\\.(ts|tsx)$': 'ts-jest'},
 	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
 	preset: 'ts-jest',
 	testEnvironment: 'node'
