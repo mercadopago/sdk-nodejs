f029171a44f579606f343d281713e7ddffe0927c
Bruna Campos <bruna.camposf@mercadolivre.com>
2023-08-24 17:57:59 -0300

    correct the types of create

--- src/clients/preferences/create/types.ts
+++ src/clients/preferences/create/types.ts
@@ -1,4 +1,5 @@
 import { MercadoPagoConfig } from '@src/mercadoPagoConfig';
+import { RedirectUrls } from '../update/types';
 
 export declare type CreatePreferenceRequest = {
   preferenceRequest: PreferenceRequest;
@@ -6,39 +7,30 @@ export declare type CreatePreferenceRequest = {
 };
 
 export declare type Identification = {
-  type: string;
-  number: string;
+  type?: string;
+  number?: string;
 };
 
 export declare type Phone = {
-  area_code: string;
-  number: number;
+  area_code?: string;
+  number?: string;
 };
 
 export declare type Address = {
-  zip_code: string;
-  street_name: string;
-  street_number: number;
+  zip_code?: string;
+  street_name?: string;
+  street_number?: number;
 };
 
-export declare type PayerRequest = {
-  name: string;
-  surname: string;
-  email: string;
-  phone: Phone;
-  identification: Identification;
-  address: Address;
-  date_created: string;
-};
-
-export declare type PayerResponse = {
-  name: string;
-  surname: string;
-  email: string;
-  phone: Phone;
-  identification: Identification;
-  address: Address;
-  date_created: string;
+export declare type Payer = {
+  name?: string;
+  surname?: string;
+  email?: string;
+  phone?: Phone;
+  identification?: Identification;
+  address?: Address;
+  date_created?: string;
+  last_purchase?: string;
 };
 
 export declare type DifferentialPricing = {
@@ -57,32 +49,30 @@ export declare type Items = {
 };
 
 export declare type ReceiverAddress = {
-  zip_code: string;
-  street_name: string;
-  city_name: string;
-  state_name: string;
-  street_number: number;
-  floor: string;
-  apartment: string;
+  zip_code?: string;
+  street_name?: string;
+  street_number?: number;
+  floor?: string;
+  apartment?: string;
+  city_name?: string;
+  state_name?: string;
+  country_name?: string;
 };
 
 export declare type FreeMethods = {
   id: number;
 };
 
-export declare type ShipmentsRequest = {
-  mode: string;
-  local_pickup: boolean;
-  dimensions: string;
-  default_shipping_method: number;
-  free_methods: Array<FreeMethods>;
-  cost: number;
-  free_shipping: boolean;
-  receiver_address: ReceiverAddress;
-};
-
-export declare type ShipmentsResponse = {
-  receiver_address: ReceiverAddress;
+export declare type Shipments = {
+  mode?: string;
+  local_pickup?: boolean;
+  dimensions?: string;
+  default_shipping_method?: number;
+  free_methods?: Array<FreeMethods>;
+  cost?: number;
+  free_shipping?: boolean;
+  receiver_address?: ReceiverAddress;
+  express_shipment?: boolean;
 };
 
 export declare type ExcludedPaymentMethods = {
@@ -94,28 +84,43 @@ export declare type ExcludedPaymentTypes = {
 };
 
 export declare type PaymentMethods = {
+  default_card_id?: string;
+  default_payment_method_id?: string;
   excluded_payment_methods?: Array<ExcludedPaymentMethods>;
   excluded_payment_types?: Array<ExcludedPaymentTypes>;
-  default_payment_method_id?: string;
-  installments?: string;
-  default_installments?: string;
+  installments?: number;
+  default_installments?: number;
+};
+
+export declare type TrackValues = {
+  conversion_id: string;
+  conversion_label: string;
+  pixel_id: string;
 };
 
 export declare type Track = {
-  type: string;
-  values: string;
+  type?: string;
+  values?: TrackValues;
 };
 
 export declare type BackUrls = {
-  success: string;
-  pending: string;
-  failure: string;
+  success?: string;
+  pending?: string;
+  failure?: string;
+};
+
+export declare type Tax = {
+  type?: string;
+  value?: number;
 };
 
 export declare type PreferenceRequest = {
   additional_info?: string;
   auto_return?: string;
   back_urls?: BackUrls;
+  binary_mode?: boolean;
+  coupon_code?: string;
+  coupon_labels?: Array<string>;
   date_of_expiration?: string;
   differential_pricing?: DifferentialPricing;
   expiration_date_from?: string;
@@ -127,35 +132,51 @@ export declare type PreferenceRequest = {
   marketplace_fee?: number;
   metadata?: NonNullable<unknown>;
   notification_url?: string;
-  payer?: PayerRequest;
+  operation_type?: string;
+  payer?: Payer;
   payment_methods?: PaymentMethods;
-  shipments?: ShipmentsRequest;
+  processing_modes?: Array<string>;
+  purpose?: string;
+  redirect_urls?: RedirectUrls;
+  shipments?: Shipments;
   statement_descriptor?: string;
+  taxes?: Array<Tax>;
   tracks?: Array<Track>;
 }
 
 export declare type PreferenceResponse = {
-  id: string;
   additional_info: string;
   auto_return: string;
   back_urls: BackUrls;
+  binary_mode: boolean;
   client_id: string;
   collector_id: number;
+  coupon_code: string;
+  coupon_labels: Array<string>;
   date_created: string;
+  date_of_expiration: string;
+  differential_pricing?: DifferentialPricing;
   expiration_date_from: string;
   expiration_date_to: string;
   expires: boolean;
   external_reference: string;
+  id: string;
   init_point: string;
+  internal_metadata: NonNullable<unknown>;
   items: Array<Items>;
   marketplace: string;
   marketplace_fee: number;
   metadata: NonNullable<unknown>;
   notification_url: string;
-  payer: PayerResponse;
-  payment_methods: PaymentMethods;
-  shipments: ShipmentsResponse;
-  statement_descriptor: string;
-  sandbox_init_point: string;
   operation_type: string;
+  payer: Payer;
+  payment_methods: PaymentMethods;
+  processing_modes: Array<string>;
+  purpose: string;
+  redirect_urls: RedirectUrls;
+  sandbox_init_point: string;
+  site_id: string;
+  shipments: Shipments;
+  statement_descriptor: string;
+  tracks: Array<Track>;
 };
