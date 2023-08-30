1ee4b7c731ba90abaf2a87f65f7d8a8199df1c54
“Lucas <lucas.duarte@mercadolivre.com>
2023-08-30 16:00:01 -0300

    rename folders camelmcase

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { MercadoPagoConfig } from './mercadoPagoConfig';
 export default MercadoPagoConfig;
 
 export { Payments } from './clients/payments';
-export { CardToken } from './clients/cardtokens';
-export { PaymentMethod } from './clients/paymentmethods';
+export { CardToken } from './clients/cardTokenss';
+export { PaymentMethod } from './clients/paymentMethodss';
 export { Preference } from './clients/preferences';
 export { CustomerCard } from './clients/customer';
