9758e125ff6cc382a0e29ba4947a89ce5e2f457e
Talita Yada <talita.yada@mercadolivre.com>
2023-09-15 17:52:42 -0300

    split UserResponse types

--- src/clients/user/get/types.ts
+++ src/clients/user/get/types.ts
@@ -13,27 +13,10 @@ export declare type UserResponse = {
   gender?: string;
   contry_id?: string;
   email: string;
-  identification: {
-    number: string;
-    type: string;
-  };
-  address: {
-    address: string | null;
-    city: string | null;
-    state: string | null;
-    zip_code: string | null;
-  };
-  phone: {
-    area_code: string;
-    extension: string;
-    number: string;
-    verified: boolean;
-  };
-  alternative_phone: {
-    area_code: string;
-    extension: string;
-    number: string;
-  };
+  identification: Identification;
+  address: Address;
+  phone: Phone;
+  alternative_phone: AlternativePhone;
   tags: string[];
   user_type?: string;
   logo?: string;
@@ -41,108 +24,147 @@ export declare type UserResponse = {
   site_id: string;
   permalink?: string;
   seller_experience?: string;
-  bill_data: {
-    accept_credit_note?: string;
-  };
-  seller_reputation: {
-    level_id: string | null;
-    power_seller_status: string | null;
-    transactions: {
-      canceled: number;
-      completed: number;
-      period: string;
-      ratings: {
-        negative: number;
-        neutral: number;
-        positive: number;
-      };
-      total: number;
-    };
-    metrics: {
-      sales: {
-        period: string;
-        completed: number;
-      };
-      claims: SellerReputationMetrics;
-      delayed_handling_time: SellerReputationMetrics;
-      cancellations: SellerReputationMetrics;
-    };
-  };
-  buyer_reputation: {
-    canceled_transactions: number;
-    tags: string[];
-    transactions: {
-      canceled: {
-        paid: number | null;
-        total: number | null;
-      };
-      completed: number | null;
-      not_yet_rated: {
-        paid: number | null;
-        total: number | null;
-        units: number | null;
-      };
-      period: string;
-      total: number | null;
-      unrated: {
-        paid: number | null;
-        total: number | null;
-      };
-    };
-  };
-  status: {
-    billing: {
-      allow: boolean;
-      codes: string[];
-    };
-    buy: {
-      allow: boolean;
-      codes: string[];
-      immediate_payment: StatusImmediate_payment;
-    };
-    confirmed_email: boolean;
-    shopping_cart: {
-      buy: string;
-      sell: string;
-    };
-    immediate_payment: boolean;
-    list: {
-      allow: boolean;
-      codes: string[];
-      immediate_payment: StatusImmediate_payment;
-    };
-    mercadoenvios: string;
-    mercadopago_account_type: string;
-    mercadopago_tc_accepted: boolean;
-    required_action: string | null;
-    sell: {
-      allow: boolean;
-      codes: string[];
-      immediate_payment: StatusImmediate_payment;
-    };
-    site_status: string;
-    user_type: string;
-  };
+  bill_data: BillData;
+  seller_reputation: SellerReputation;
+  buyer_reputation: BuyerReputation;
+  status: Status;
   secure_email?: string;
-  company: {
-    brand_name: string;
-    city_tax_id: string;
-    corporate_name: string;
-    identification: string;
-    state_tax_id: string;
-    cust_type_id: string;
-    soft_descriptor: string;
-  };
-  credit: {
-    consumed: number;
-    credit_level_id: string;
-    rank: string;
-  };
-  context: {
-    ip_address: string;
-  };
+  company: Company;
+  credit: Credit;
+  context: Context;
   registration_identifiers: string[];
   country_id: string;
+};
+
+export declare type Identification = {
+  number: string;
+  type: string;
+};
+
+export declare type Address = {
+  address: string | null;
+  city: string | null;
+  state: string | null;
+  zip_code: string | null;
+};
+
+export declare type Phone = {
+  area_code: string;
+  extension: string;
+  number: string;
+  verified: boolean;
+};
+
+export declare type AlternativePhone = {
+  area_code: string;
+  extension: string;
+  number: string;
+};
+
+export declare type BillData = {
+  accept_credit_note?: string;
+};
+
+export declare type SellerReputation = {
+  level_id: string | null;
+  power_seller_status: string | null;
+  transactions: {
+    canceled: number;
+    completed: number;
+    period: string;
+    ratings: {
+      negative: number;
+      neutral: number;
+      positive: number;
+    };
+    total: number;
+  };
+  metrics: {
+    sales: {
+      period: string;
+      completed: number;
+    };
+    claims: SellerReputationMetrics;
+    delayed_handling_time: SellerReputationMetrics;
+    cancellations: SellerReputationMetrics;
+  };
+};
+
+export declare type BuyerReputation = {
+  canceled_transactions: number;
+  tags: string[];
+  transactions: {
+    canceled: {
+      paid: number | null;
+      total: number | null;
+    };
+    completed: number | null;
+    not_yet_rated: {
+      paid: number | null;
+      total: number | null;
+      units: number | null;
+    };
+    period: string;
+    total: number | null;
+    unrated: {
+      paid: number | null;
+      total: number | null;
+    };
+  };
+};
+
+export declare type Status = {
+  billing: {
+    allow: boolean;
+    codes: string[];
+  };
+  buy: {
+    allow: boolean;
+    codes: string[];
+    immediate_payment: StatusImmediatePayment;
+  };
+  confirmed_email: boolean;
+  shopping_cart: {
+    buy: string;
+    sell: string;
+  };
+  immediate_payment: boolean;
+  list: {
+    allow: boolean;
+    codes: string[];
+    immediate_payment: StatusImmediatePayment;
+  };
+  mercadoenvios: string;
+  mercadopago_account_type: string;
+  mercadopago_tc_accepted: boolean;
+  required_action: string | null;
+  sell: {
+    allow: boolean;
+    codes: string[];
+    immediate_payment: StatusImmediatePayment;
+  };
+  site_status: string;
+  user_type: string;
+};
+
+export declare type Company = {
+  brand_name: string;
+  city_tax_id: string;
+  corporate_name: string;
+  identification: string;
+  state_tax_id: string;
+  cust_type_id: string;
+  soft_descriptor: string;
+};
+
+export declare type Credit = {
+  consumed: number;
+  credit_level_id: string;
+  rank: string;
+};
+
+export declare type Context = {
+  ip_address: string;
 };
 
 export declare type SellerReputationMetrics = {
@@ -151,7 +173,7 @@ export declare type SellerReputationMetrics = {
   value: number;
 };
 
-export declare type StatusImmediate_payment = {
+export declare type StatusImmediatePayment = {
   reasons: string[];
   required: boolean;
 };
