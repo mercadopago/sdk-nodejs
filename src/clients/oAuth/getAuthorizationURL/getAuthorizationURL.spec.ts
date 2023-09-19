8b3fc801ce764d54a686381539527cd9545ba64c
Bruna Campos <bruna.camposf@mercadolivre.com>
2023-09-19 15:32:25 -0300

    modify getAuthorizationURL test

--- src/clients/oAuth/getAuthorizationURL/getAuthorizationURL.spec.ts
+++ src/clients/oAuth/getAuthorizationURL/getAuthorizationURL.spec.ts
@@ -1,10 +1,7 @@
 import getAuthorizationURL from '.';
-import { RestClient } from '@utils/restClient';
-
-jest.mock('@utils/restClient');
 
 describe('Testing oAuth, getAuthorizationURL', () => {
-	test('should append query params to url ', async () => {
+	test('getAuthorizationURL should correctly append query params to URL', async () => {
 
 		const authorization = {
 			filters: {
@@ -14,17 +11,8 @@ describe('Testing oAuth, getAuthorizationURL', () => {
 			}
 		};
 
-		getAuthorizationURL(authorization);
-		const spyFetch = jest.spyOn(RestClient, 'appendQueryParamsToUrl');
-		expect(spyFetch).toHaveBeenCalledWith(
-			'https://auth.mercadopago.com/authorization',
-			{
-				client_id: 'string',
-				response_type: 'code',
-				platform_id: 'mp',
-				state: '12345',
-				redirect_uri: 'https://httpdump.app/dumps/74cf0638-1a20-4482-b22d-2662a135cfd0'
-			},
-		);
+		const authorizationURL = getAuthorizationURL(authorization);
+		expect(authorizationURL)
+			.toBe('https://auth.mercadopago.com/authorization?client_id=string&state=12345&redirect_uri=https%3A%2F%2Fhttpdump.app%2Fdumps%2F74cf0638-1a20-4482-b22d-2662a135cfd0&response_type=code&platform_id=mp');
 	});
 });
