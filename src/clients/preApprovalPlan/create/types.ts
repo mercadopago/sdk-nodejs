c64a2ebd6348ef4d4d413e8597848c1a2ed2a116
“Lucas <lucas.duarte@mercadolivre.com>
2023-10-02 19:05:38 -0300

    typo issues

--- src/clients/preApprovalPlan/create/types.ts
+++ src/clients/preApprovalPlan/create/types.ts
@@ -2,12 +2,12 @@ import type { MercadoPagoConfig } from '@src/mercadoPagoConfig';
 import type { PreApprovalPlanRequest } from '@src/clients/preApprovalPlan/commonTypes';
 import type { Options } from '@src/types';
 
-export declare type PreApprovalPlansCreateClient = {
+export declare type PreApprovalPlanCreateClient = {
   body: PreApprovalPlanRequest;
   config: MercadoPagoConfig
 };
 
-export declare type PreApprovalPlansCreateData = {
+export declare type PreApprovalPlanCreateData = {
   body: PreApprovalPlanRequest;
   requestOptions?: Options;
 }
