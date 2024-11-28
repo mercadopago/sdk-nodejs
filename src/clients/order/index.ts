1580209609d18f7f2f083a12e3c758462ad741b9
andreagostinho-meli <andre.agostinho@mercadolivre.com>
2024-11-28 13:59:10 -0300

    fix: import OrderResponse correctly

--- src/clients/order/index.ts
+++ src/clients/order/index.ts
@@ -1,7 +1,8 @@
 import create from './create';
 
 import type { MercadoPagoConfig } from '@src/mercadoPagoConfig';
-import { CreateOrderData, OrderResponse } from './create/types';
+import { CreateOrderData } from './create/types';
+import { OrderResponse } from './commonTypes';
 
 /**
  * Mercado Pago Order.
@@ -9,19 +10,19 @@ import { CreateOrderData, OrderResponse } from './create/types';
  * @see {@link https://www.mercadopago.com/developers/en/reference Documentation }.
  */
 export class Order {
-	private config: MercadoPagoConfig;
+  private config: MercadoPagoConfig;
 
-	constructor(mercadoPagoConfig: MercadoPagoConfig) {
-		this.config = mercadoPagoConfig;
-	}
+  constructor(mercadoPagoConfig: MercadoPagoConfig) {
+    this.config = mercadoPagoConfig;
+  }
 
-	/**
+  /**
    * Create Order.
    *
    * @see {@link https://github.com/mercadopago/sdk-nodejs/blob/master/src/examples/order/create.ts Usage Example }.
    */
-	create({ body, requestOptions }: CreateOrderData): Promise<OrderResponse> {
-		this.config.options = { ...this.config.options, ...requestOptions };
-		return create({ body, config: this.config });
-	}
+  create({ body, requestOptions }: CreateOrderData): Promise<OrderResponse> {
+    this.config.options = { ...this.config.options, ...requestOptions };
+    return create({ body, config: this.config });
+  }
 }
