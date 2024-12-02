674812d522dc802eab7d95ce868ad7481e67f0f6
andreagostinho-meli <andre.agostinho@mercadolivre.com>
2024-12-02 18:04:24 -0300

    feat: add createTransaction function

---
+++ src/clients/order/transaction/create/index.ts
@@ -0,0 +1,17 @@
+import { RestClient } from '@src/utils/restClient';
+import { TransactionsResponse } from '../../commonTypes';
+import { CreateOrderTransactionClient } from './types';
+
+export default function createTransaction({ id, body, config }: CreateOrderTransactionClient): Promise<TransactionsResponse> {
+	return RestClient.fetch<TransactionsResponse>(
+		`v1/orders/${id}/transactions`,
+		{
+			method: 'POST',
+			headers: {
+				'Authorization': `Bearer ${config.accessToken}`
+			},
+			body: JSON.stringify(body),
+			...config.options
+		}
+	);
+}
