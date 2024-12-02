7cd444e0c49f2e5675884f0b26a46a1f543cb054
andreagostinho-meli <andre.agostinho@mercadolivre.com>
2024-12-02 18:03:44 -0300

    feat: map create transaction request

---
+++ src/clients/order/transaction/create/types.ts
@@ -0,0 +1,33 @@
+// API version: 5d077b6f-61b2-4b3a-8333-7a64ee547448
+
+import { MercadoPagoConfig } from '@src/mercadoPagoConfig';
+import type { Options } from '@src/types';
+
+export declare type CreateOrderTransactionClient = {
+	id: string;
+	body: CreateOrderTransactionRequest;
+	config: MercadoPagoConfig;
+}
+
+export declare type CreateOrderData = {
+	id: string;
+	body: CreateOrderTransactionRequest;
+	requestOptions?: Options;
+}
+
+export declare type CreateOrderTransactionRequest = {
+	payments?: PaymentRequest[];
+}
+
+export declare type PaymentRequest = {
+	amount?: string;
+	payment_method?: PaymentMethodRequest;
+}
+
+export declare type PaymentMethodRequest = {
+	id?: string;
+	type?: string;
+	token?: string;
+	installments?: number;
+	statement_descriptor?: string;
+}
