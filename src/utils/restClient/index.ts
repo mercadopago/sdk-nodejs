9ef2eb43cf74ef1b42d9460d0fb535f16489abc8
Mauro-Domingues <maurinho.villa@hotmail.com>
2025-03-17 15:48:20 -0300

    fix(query-params): add validation to ensure key belongs to queryParams object

--- src/utils/restClient/index.ts
+++ src/utils/restClient/index.ts
@@ -20,7 +20,7 @@ class RestClient {
 		const searchParams = new URLSearchParams();
 
 		for (const key in queryParams) {
-			if (typeof queryParams?.[key] !== 'undefined') {
+			if (Object.prototype.hasOwnProperty.call(queryParams,key) && typeof queryParams[key] !== 'undefined') {
 				searchParams.append(key, queryParams[key].toString());
 			}
 		}
