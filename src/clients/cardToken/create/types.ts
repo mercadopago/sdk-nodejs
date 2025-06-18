55809f60ff8948931cc06f282b750e9c26ead848
andreagostinho-meli <andre.agostinho@mercadolivre.com>
2025-06-18 11:51:31 -0300

    fix: add card_number, expiration_month and expiration_year

--- src/clients/cardToken/create/types.ts
+++ src/clients/cardToken/create/types.ts
@@ -2,17 +2,20 @@ import type { MercadoPagoConfig } from '@src/mercadoPagoConfig';
 import { Options } from '@src/types';
 
 export declare type CardTokenCreateClient = {
-  body: CardTokenCreateBody;
-  config: MercadoPagoConfig;
+	body: CardTokenCreateBody;
+	config: MercadoPagoConfig;
 };
 
 export declare type CardTokenCreateBody = {
-  card_id?: string;
-  customer_id?: string;
-  security_code?: string;
+	card_id?: string;
+	card_number?: string;
+	customer_id?: string;
+	expiration_month?: string;
+	expiration_year?: string;
+	security_code?: string;
 };
 
 export declare type CardTokenCreateData = {
-  body: CardTokenCreateBody;
-  requestOptions?: Options;
+	body: CardTokenCreateBody;
+	requestOptions?: Options;
 };
