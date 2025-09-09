899afe204c872c0ff76ed37dd091c8b80cfc538a
aqudsilva <raquel.rdsilva@mercadolivre.com>
2025-09-09 15:13:11 -0300

    feat: adding order fields/fixing additional info

--- src/clients/order/create/types.ts
+++ src/clients/order/create/types.ts
@@ -25,81 +25,12 @@ export declare type CreateOrderRequest = {
 	config?: Config;
 	checkout_available_at?: string;
 	expiration_time?: string;
-	additional_info?: AdditionalInfo;
+	currency?: string;
+	additional_info?: Record<string, any>;
 };
 export declare type TransactionsRequest = {
 	payments?: PaymentRequest[];
 };
-export interface AdditionalInfo {
-	payer?: {
-		authentication_type?: string;
-		registration_date?: string;
-		is_prime_user?: boolean;
-		is_first_purchase_online?: boolean;
-		last_purchase?: string;
-	};
-	shipment?: {
-		express?: boolean;
-		local_pickup?: boolean;
-	};
-	platform?: {
-		shipment?: {
-			delivery_promise?: string;
-			drop_shipping?: string;
-			safety?: string;
-			tracking?: {
-				code?: string;
-				status?: string;
-				withdrawn?: boolean;
-			};
-		};
-		seller?: {
-			id?: string;
-			name?: string;
-			email?: string;
-			status?: string;
-			referral_url?: string;
-			registration_date?: string;
-			hired_plan?: string;
-			business_type?: string;
-			address?: {
-				zip_code?: string;
-				street_name?: string;
-				street_number?: string;
-				city?: string;
-				state?: string;
-				complement?: string;
-				country?: string;
-			};
-			identification?: {
-				type?: string;
-				number?: string;
-			};
-			phone?: {
-				area_code?: string;
-				number?: string;
-			};
-		};
-		authentication?: string;
-	};
-	travel?: {
-		passengers?: Array<{
-			first_name: string;
-			last_name: string;
-			identification: {
-				type: string;
-				number: string;
-			};
-		}>;
-		routes?: Array<{
-			departure: string;
-			destination: string;
-			departure_date_time?: string;
-			arrival_date_time?: string;
-			company: string;
-		}>;
-	};
-}
 export declare type PaymentRequest = {
 	amount?: string;
 	payment_method?: PaymentMethodRequest;
